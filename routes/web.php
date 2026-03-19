b4d5a5756db8fa31bc6c4da31512542e98110d7e
Eli Alejandro <iscelialejandro@gmail.com>
2026-03-19 08:53:48 -0600

    Add a new route for '/hello' that returns a JSON response with a greeting message

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/admin/login');
 });
+
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'Hello, World!',
+    ]);
+});
